7b154fa9986c9919b5f126080f14e9cbe2902366
4Ritaa <nnrtchege@gmail.com>
2024-06-14 14:39:36 +0300

    new ecommerce project updates

--- plp_ecommerce/views.py
+++ plp_ecommerce/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,get_object_or_404
+from .models import Customer,Product
 
 # Create your views here.
 def product_list(request):
@@ -8,3 +8,24 @@ def product_list(request):
         'products': products
     }
     return render(request,'plp_ecommerce/product_list.html',context)
+
+def product_detail(request, pk):
+    product = get_object_or_404
+    context = {
+        'product': product
+    }
+    return render(request,'plp_ecommerce/product_detail.html',context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers' : customers
+    }
+    return render(request,'plp_ecommerce/customer_list.html',context)
+
+def customer_detail(request,pk):
+    customer = get_object_or_404(Customer,pk=pk)
+    context = {
+        'customer': customer
+    }
+    return render(request,'plp_ecommerce/customer_detail.html',context)
